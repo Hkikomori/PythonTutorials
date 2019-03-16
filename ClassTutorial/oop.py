c852037714254421fb4c3c9279bc94f22ddb1b13
Hikikomori <norman.catalan.fr@gmail.com>
2019-03-16 16:57:41 +0100

    static and class methods

--- ClassTutorial/oop.py
+++ ClassTutorial/oop.py
@@ -1,7 +1,5 @@
 # https://www.youtube.com/watch?v=ZDa-Z5JzLYM&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc
 
-import numpy as np
-
 
 class Employee:
     # Class Variables, can be accased from the class or an instance
@@ -15,6 +13,7 @@ class Employee:
         self.pay = pay
         self.email = '{}.{}@company-name.com'.format(self.first, self.last)
         self.fullname = '{} {}'.format(self.first, self.last)
+
         Employee.num_of_employees += 1
 
     # Methods
@@ -24,9 +23,25 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    # Alternative Constructor
+    @classmethod
+    def from_string(cls, emp_string):
+        first, last, pay = emp_string.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(date):
+        return date.weekday() != 5 and date.weekday() != 6
+
 
 def Main():
-    pass
+    import datetime
+    my_date = datetime.date(2019, 3, 18)
+    print(Employee.is_workday(my_date))
 
 
 if __name__ == '__main__':
