a7ac56e682d9f066aee7708272df1725f283136d
Hikikomori <norman.catalan.fr@googlemail.com>
2019-05-19 19:18:25 +0200

    started morpheus gui tutorial

--- GUI/py_gui.py
+++ GUI/py_gui.py
@@ -1,13 +1,41 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5 import QtCore
+
+class Fenster(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initMe()
+
+    def initMe(self):
+        self.setGeometry(50, 50, 500, 500)
+        self.setWindowTitle('My First GUI')
+        self.setWindowIcon(QIcon('Icons/icon.png'))
+
+        button = QPushButton('Click Me !',self)
+        button.move(50,50)
+        QToolTip.setFont(QFont('Arial', 8))
+        button.setToolTip('<b>Nothing</b> will happen')
+        button.clicked.connect(self.button_pressed)
+
+        quit = QPushButton('Exit',self)
+        quit.move(200,200)
+        quit.setToolTip('Exit GUI')
+        quit.clicked.connect(self.button_exit)
+
+        self.show()
+
+    def button_pressed(self):
+        print('Hello World!')
+
+    def button_exit(self):
+        btn = self.sender()
+        print('Exiting GUI')
+        #QtCore.QCoreApplication.instance().quit
+
+
 
 app = QApplication(sys.argv)
-
-w = QWidget()
-w.setGeometry(50, 50, 500, 500)
-w.setWindowTitle('My First GUI')
-w.setWindowIcon(QIcon('Icons/icon.png'))
-w.show()
-
+w = Fenster()
 sys.exit(app.exec_())
