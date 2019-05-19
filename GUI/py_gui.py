fa10d2874a33cc9f6082b5a9afd93ce6bf567731
Hikikomori <norman.catalan.fr@googlemail.com>
2019-05-19 18:49:45 +0200

    started morpheus gui tutorial

--- GUI/py_gui.py
+++ GUI/py_gui.py
@@ -1,4 +1,13 @@
-import os
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-def Main():
-    pass
+app = QApplication(sys.argv)
+
+w = QWidget()
+w.setGeometry(50, 50, 500, 500)
+w.setWindowTitle('My First GUI')
+w.setWindowIcon(QIcon('Icons/icon.png'))
+w.show()
+
+sys.exit(app.exec_())
